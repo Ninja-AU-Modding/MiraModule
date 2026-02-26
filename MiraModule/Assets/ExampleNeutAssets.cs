d4a7fafb9f07c85b196679f60ba8e6977f57c4bf
notnico22 <notnicotv@gmail.com>
2026-02-26 07:38:43 -0500

    Update ExampleNeutAssets.cs

--- MiraModule/Assets/ExampleNeutAssets.cs
+++ MiraModule/Assets/ExampleNeutAssets.cs
@@ -11,6 +11,6 @@ public static class ExampleNeutAssets
     public static LoadableAsset<Sprite> SentinelExplodeSprite { get; } = new LoadableResourceAsset($"{ShortPath}.SentinelExplodeButton.png");
     public static LoadableAsset<Sprite> SentinelKillSprite { get; } = new LoadableResourceAsset($"{ShortPath}.SentinelKillButton.png");
 
-    // Pelican Assets
-    public static LoadableAsset<Sprite> PelicanGulpSprite { get; } = new LoadableResourceAsset($"{ShortPath}.Tempbutton.png");
+    // Abyss Assets
+    public static LoadableAsset<Sprite> AbyssGulpSprite { get; } = new LoadableResourceAsset($"{ShortPath}.AbyssGulpButton.png");
 }
