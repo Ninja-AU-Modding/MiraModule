9f415b553b707a294cfa60239e0b5bec4078a93d
notnico22 <notnicotv@gmail.com>
2026-02-26 06:47:59 -0500

    Create MiraModulePlayerColors.cs

---
+++ MiraModule/MiraModulePlayerColors.cs
@@ -0,0 +1,11 @@
+using TownOfUs;
+using UnityEngine;
+
+namespace MiraModule;
+
+public static class MiraModulePlayerColors
+{
+    // Add custom player colors here if needed
+    // Example:
+    // public static Color CustomColor => new Color32(255, 100, 100, 255);
+}
