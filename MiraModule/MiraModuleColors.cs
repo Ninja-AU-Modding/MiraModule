e9dbaa239f3bbd8a5db7a7e743f29dde43522478
notnico22 <notnicotv@gmail.com>
2026-02-26 06:47:58 -0500

    Create MiraModuleColors.cs

---
+++ MiraModule/MiraModuleColors.cs
@@ -0,0 +1,15 @@
+using MiraAPI.Utilities;
+using TownOfUs;
+using UnityEngine;
+
+namespace MiraModule;
+
+public static class MiraModuleColors
+{
+    // Crew Colors
+    public static Color Chameleon => TownOfUsColors.UseBasic ? Palette.CrewmateBlue : new Color32(81, 180, 154, 255);
+    
+    // Neutral Colors
+    public static Color Sentinel => new Color32(143, 162, 141, 255);
+    public static Color Hacker => new Color32(0, 255, 127, 255); // Bright green/cyan hacker color
+}
