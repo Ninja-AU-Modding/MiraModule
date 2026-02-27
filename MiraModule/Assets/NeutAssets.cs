b1fe4fd22873a8d3d3ba425113a0f47b525e6702
notnico22 <notnicotv@gmail.com>
2026-02-26 23:46:22 -0500

    Update NeutAssets.cs

--- MiraModule/Assets/NeutAssets.cs
+++ MiraModule/Assets/NeutAssets.cs
@@ -5,15 +5,10 @@ namespace MiraModule.Assets;
 
 public static class NeutAssets
 {
-    // Sentinel Assets
     private const string ShortPath = "MiraModule.Resources.NeutButtons";
     public static LoadableAsset<Sprite> SentinelVentSprite { get; } = new LoadableResourceAsset($"{ShortPath}.SentinelVentButton.png");
     public static LoadableAsset<Sprite> SentinelExplodeSprite { get; } = new LoadableResourceAsset($"{ShortPath}.SentinelExplodeButton.png");
     public static LoadableAsset<Sprite> SentinelKillSprite { get; } = new LoadableResourceAsset($"{ShortPath}.SentinelKillButton.png");
-
-    // Abyss Assets
     public static LoadableAsset<Sprite> AbyssGulpSprite { get; } = new LoadableResourceAsset($"{ShortPath}.AbyssGulpButton.png");
-
-    // Shifter Assets
     public static LoadableAsset<Sprite> ShifterShiftSprite { get; } = new LoadableResourceAsset($"{ShortPath}.Tempbutton.png");
 }
