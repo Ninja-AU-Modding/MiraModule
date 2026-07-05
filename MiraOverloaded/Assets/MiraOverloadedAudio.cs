0decdeade56d0b69db4dad3d8fd171c7b047d1a6
GoobisMoobis <fenbrokam@icloud.com>
2026-07-05 03:55:36 -0400

    Add Another New Sound + Add Sounds to Assets

--- MiraOverloaded/Assets/MiraOverloadedAudio.cs
+++ MiraOverloaded/Assets/MiraOverloadedAudio.cs
@@ -10,4 +10,10 @@ public static class MiraOverloadedAudio
     private const string ShortPath = "MiraOverloaded.Resources.Audio";
     public static LoadableAsset<AudioClip> HiveMindIntro =>
         new LoadableAudioResourceAsset($"{ShortPath}.HiveMindIntro.wav");
+    public static LoadableAsset<AudioClip> HiveMindAwaken =>
+        new LoadableAudioResourceAsset($"{ShortPath}.HiveMindAwaken.wav");
+    public static LoadableAsset<AudioClip> HiveMindKilled =>
+        new LoadableAudioResourceAsset($"{ShortPath}.HiveMindKilled.wav");
+    public static LoadableAsset<AudioClip> HiveMindDeath =>
+        new LoadableAudioResourceAsset($"{ShortPath}.HiveMindDeath.wav");
 }
